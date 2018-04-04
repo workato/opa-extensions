cf53be3660c5e314a389ef35a1390baf849d126c
maxim.bazhenov <maxim.bazhenov@lifestreet.com>
2018-04-04 14:28:41 +0300

    NTLM Headers support

--- src/main/java/com/mycompany/onprem/NtlmExtension.java
+++ src/main/java/com/mycompany/onprem/NtlmExtension.java
@@ -1,12 +1,9 @@
 package com.mycompany.onprem;
 
-import java.net.URL;
-import java.util.Map;
-
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
 import org.apache.http.HttpStatus;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.NTCredentials;
@@ -20,7 +17,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,35 +25,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class NtlmExtension {
 
+	private static final String HEADER_WORKATO_URL = "X-Workato-NTLM-url";
+
+	// username:password@domain/workstation
+	private static final String HEADER_WORKATO_AUTH = "X-Workato-NTLM-auth";
+
 	@Inject
 	private Environment env;
 
 	private CloseableHttpClient httpClient = HttpClients.createDefault();
 
 	@RequestMapping(path = "/request", method = RequestMethod.POST)
-	public @ResponseBody String ntlmUrl(@RequestBody Map<String, Object> body) {
-		String username = (String)body.get("username");
-		String password = (String)body.get("password");
-		String url = (String)body.get("url");
+	public @ResponseBody String request(@RequestHeader(HEADER_WORKATO_AUTH) String authHeader,
+										@RequestHeader(HEADER_WORKATO_URL) String urlHeader,
+										HttpServletResponse httpServletResponse) {
 
-		if (url != null) {
+		NtlmAuth auth = NtlmAuth.build(authHeader);
+
+		if (auth.username != null && auth.password != null) {
 			try {
-				URL urlObj = new URL(url);
-
 				CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-				credentialsProvider.setCredentials(AuthScope.ANY, new NTCredentials(username, password, null, null));
-
-				HttpHost target = new HttpHost(urlObj.getHost(), urlObj.getPort(), urlObj.getProtocol());
+				credentialsProvider.setCredentials(AuthScope.ANY, new NTCredentials(auth.username, auth.password, auth.workstation, auth.domain));
 
 				HttpClientContext context = HttpClientContext.create();
 				context.setCredentialsProvider(credentialsProvider);
 
-				HttpGet httpGet = new HttpGet(urlObj.getFile());
+				HttpGet httpGet = new HttpGet(urlHeader);
 
-				try (CloseableHttpResponse response = httpClient.execute(target, httpGet, context)) {
+				try (CloseableHttpResponse response = httpClient.execute(httpGet, context)) {
 					HttpEntity entity = response.getEntity();
 					if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 						return EntityUtils.toString(entity);
+					} else {
+						httpServletResponse.setStatus(response.getStatusLine().getStatusCode());
 					}
 				}
 			} catch (Exception e) {
@@ -64,7 +65,43 @@ public class NtlmExtension {
 			}
 		}
 
-		return "";
+		return null;
 	}
 
+	private static class NtlmAuth {
+		private String username;
+		private String password;
+		private String domain;
+		private String workstation;
+
+		private static NtlmAuth build(String authHeader) {
+			NtlmAuth auth = new NtlmAuth();
+
+			String[] authSplit = authHeader.split("@");
+			if (authSplit.length == 2) {
+				buildCredentials(auth, authSplit[0]);
+				if (auth.username != null) {
+					String[] domainSplit = authSplit[1].split("/");
+					if (domainSplit.length == 2) {
+						auth.domain = domainSplit[0];
+						auth.workstation = domainSplit[1];
+					} else if (domainSplit.length == 1) {
+						auth.domain = domainSplit[0];
+					}
+				}
+			} else if (authSplit.length == 1) {
+				buildCredentials(auth, authSplit[0]);
+			}
+
+			return auth;
+		}
+
+		private static void buildCredentials(NtlmAuth auth, String input) {
+			String[] credSplit = input.split(":");
+			if (credSplit.length == 2) {
+				auth.username = credSplit[0];
+				auth.password = credSplit[1];
+			}
+		}
+	}
 }
